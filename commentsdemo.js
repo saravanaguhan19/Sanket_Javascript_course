11c39a1bb044be0909d090614dc5854f4a65bc08
saravanaguhan19 <saravanaguhan19@gmail.com>
2023-11-04 21:28:39 +0530

    till comment

---
+++ commentsdemo.js
@@ -0,0 +1,11 @@
+console.log("Hello World");
+
+//this is a comment 
+
+console.log("Ending");
+
+/* this 
+is a 
+multiline comment */
+
+console.log("finally");
--- specialCharacter.js
+++ specialCharacter.js
@@ -1,2 +1,5 @@
 var text = "this is some random \t text we will print in next line";
 console.log(text);
+
+var text1 = "this is some random \n text we will print in next line";
+console.log(text1);
