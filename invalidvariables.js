47762b2eeccaa42a959d73efa554f417a94f39f6
saravanaguhan19 <saravanaguhan19@gmail.com>
2023-11-04 18:27:55 +0530

    variable demo done

---
+++ invalidvariables.js
@@ -0,0 +1,7 @@
+// const first name = 10;
+
+// const 3address = 23 ;
+
+const address3 = 34;
+
+const addr333res = 32;
---
+++ variabledemo.js
@@ -0,0 +1,13 @@
+var score = 10;
+
+var mark = 90;
+
+let age = 24;
+let flag = 0;
+
+const x = 100;
+
+x = 24;
+
+console.log(x);
+console.log(score);
