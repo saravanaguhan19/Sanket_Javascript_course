a2745955d9b4c93dde7c737ecfb0a4fd60dd00ac
saravanaguhan19 <saravanaguhan19@gmail.com>
2023-11-05 07:12:48 +0530

    assignment operator

---
+++ operatorsdemo.js
@@ -0,0 +1,28 @@
+//arthmetic operator
+let x = 10;
+let y = 3;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+console.log(y ** 2);
+
+//assignment operator
+
+let a = 10;
+a += 7;
+console.log("value of a : " , a);
+
+a %= 3;
+console.log("value of a after modulo is ", a);
+
+a += 3;
+console.log("value of a after addition is :" , a);
+
+a *= 2;
+console.log("value of a after multiplication is :",a);
+
+a **= 2 ;
+console.log("value of a after exponent is :",a);
