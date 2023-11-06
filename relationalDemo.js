8605851c50ceb7282b5d065d5f76e644e3f835a7
saravanaguhan19 <saravanaguhan19@gmail.com>
2023-11-06 20:18:52 +0530

    special number

---
+++ relationalDemo.js
@@ -0,0 +1,25 @@
+let x = 10;
+let y = 20;
+let z = 5;
+let a = 10;
+
+console.log("x < z", x < z);
+console.log("y > z", y > z);
+console.log("x <= z", x <= z);
+console.log("x >= a", x >= a);
+
+//logical operator
+
+console.log(true && false);
+console.log(false || false);
+console.log(true && true);
+console.log(5 < 10 && 6 < 3);
+console.log(!false);
+
+console.log(10 && 6);
+
+console.log(6 && 10);
+
+console.log(1000 || null);
+
+console.log(false || undefined);
---
+++ specialnumbers.js
@@ -0,0 +1,11 @@
+console.log(+0);
+console.log(-0);
+console.log(NaN);
+console.log(+Infinity);
+console.log(-Infinity);
+
+console.log(5 | 7);
+
+console.log((6 & 9) , (6 | 9));
+
+
